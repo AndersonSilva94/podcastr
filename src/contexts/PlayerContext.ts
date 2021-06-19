f2e22abdc8379f616b26cb1fac569438b92ffa0e
andy silva <andssilva.23@gmail.com>
2021-06-19 01:42:42 -0400

    feat: :rocket: day 4 - NLW5 (rocketseat)

---
+++ src/contexts/PlayerContext.ts
@@ -0,0 +1,25 @@
+import { createContext } from 'react';
+
+type Episode = {
+  title: string,
+  members: string,
+  thumbnail: string,
+  duration: number,
+  url: string,
+}
+
+type PlayerContextData = {
+  episodeList: Array<Episode>,
+  currentEpisodeIndex: number,
+  isPlaying: boolean,
+  play: (episode: Episode) => void;
+  togglePlay: () => void;
+  setPlayingState: (state: boolean) => void;
+}
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
+/* export const PlayerContext = createContext({
+  episodeList: [],
+  currentEpisodeIndex: 0,
+}) */
