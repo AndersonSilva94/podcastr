911f4cccdffc58b36ff203174e395ec0d6066eb0
andy silva <andssilva.23@gmail.com>
2021-06-20 02:47:05 -0400

    feat: :rocket: add README file

---
+++ README.md
@@ -0,0 +1,3 @@
+![imagem](https://t2.tudocdn.net/572277?w=900&h=284)
+# NLW#5 - Trilha de React.Js
+## Projeto Podcastr
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -122,8 +122,8 @@ export const getStaticProps: GetStaticProps = async () => {
   const { data } = await api.get('episodes', {
     params: {
       _limit: 12,
-      /* _sort: 'published_at', */
-      order: 'desc'
+      _sort: 'published_at',
+      _order: 'desc'
     }
   });
 
